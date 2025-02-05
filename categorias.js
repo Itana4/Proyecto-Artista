56dbfd82b2faa99e0f5c18e3707662b46e620f82
sandriicola <sandra.sansan16@gmail.com>
2025-02-05 18:51:33 +0100

    fetch en categorias.js

---
+++ categorias.js
@@ -0,0 +1,11 @@
+const getArtistas = () => fetch("http://localhost:3000/artistas")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    console.error("Ocurrió un error:", error.message);
+    throw new Error("Error al cargar los perfiles");
+  });
